36380d4431a27bf8664ef82e68c87e1b2389e03f
Moshe Levi <mo6709@gmail.com>
2018-05-29 09:05:43 -0400

    add post route for blocks

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -13,37 +13,11 @@ router.get('/api/blocks', (req, res) => {
 
 
 router.post('/api/blocks', (req, res) => {
-    //Validate
-    // const schema = {
-    //     snederId: Joi.string().min(2).required()
-    // };
-
-    // const result = Joi.validate(req.body, schema);
-
-    // if(result.error) {
-    // 	res.status(400).send(result.error.details);
-    // 	result;
-    // }
-    
-
-   //  const trassertan = { 
-   //  	id: transactions.length + 1, 
-  	//     snederId: req.body.snederId ,
-  	//     reciverId: req.body.reciverId,
-  	//     amount: 100 
-  	// }
-
-    // let transactionInfo = {};
-    // Blockchain.createTransaction(transactionInfo);
-    // const minedTran = Blockchain.miningPendingTransactions("address");
-    // res.status(200).send(minedTran);
+    const blockchain = new Blockchain();
+    let transactionInfo = req.body;
+    blockchain.createTransaction(transactionInfo);
+    const minedTran = blockchain.miningPendingTransactions("address");
+    res.status(200).send(minedTran);
 });
 
 module.exports = router;
-
-// router.get('/api/blocks/:id', (req, res, next) => { 
-//  const id  = req.params.id;
-//  const tran = transactions.find( tran => tran.id === partInt(id));
-//  if(!tran) res.status(404).send("Could not find transaction");
-//  res.send(tran);
-// });
