07553b636409a080c3ae9c8236d50b2d7202ca6c
Moshe Levi <mo6709@gmail.com>
2018-05-30 13:54:48 -0400

    update routes/api/transaction

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -1,5 +1,6 @@
 const Blockchain = require('../../models/blockchain/blockchain');
 const Block = require('../../models/blockchain/block');
+const Transaction = require('../../models/transaction');
 const globalBlock = new Block();
 const globalBlockchain = new Blockchain();
 
@@ -13,8 +14,10 @@ router.get('/api/blocks', (req, res) => {
 
 router.post('/api/blocks', (req, res) => {
     let transactionInfo = req.body.tran;
-    globalBlockchain.createTransaction(transactionInfo);
-    const minedTran = globalBlockchain.miningPendingTransactions("address");
+    const transaction = new Transaction(transactionInfo);
+    globalBlockchain.createTransaction(transaction);
+    //Don't forget to update the mining proccess
+    const minedTranHash = globalBlockchain.miningPendingTransactions("address");
     res.status(200).send(minedTran);
 });
 
